f955ea8ae01c153cc0384370a9566b32cfea2d2b
Karim Dabboussi <kmdabboussi@my.canyons.edu>
2017-10-27 12:04:08 -0700

    Point System Update 2
    Adeded a score counter on the ui and gave more relavent descriptions.

--- CompSciClubFall2017/Assets/Scripts/PointSystem.cs
+++ CompSciClubFall2017/Assets/Scripts/PointSystem.cs
@@ -10,46 +10,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour {
+    public Text scoreText;
+    public float scoreTimer;
+    public float scoreDelayTimer = 0.5f;
     public int point = 0;
-    int score = 0;
-    const int ENEMY1 = 100;
-    const int ENEMY2 = 100;
-    const int BOSS1 = 1000;
-    const int BOSS2 = 2000;
+    const int ENEMY1 = 100; //possible values for later
+    const int ENEMY2 = 100; //possible values for later
+    const int BOSS1 = 1000; //possible values for later
+    const int BOSS2 = 2000; //possible values for later
     // Use this for initialization
     void Start () {
+        scoreText.text = point.ToString();
+        scoreTimer = scoreDelayTimer;
        
     }
 
     // Update is called once per frame
     void Update () {
-        point = point + 100; // will delete later
+        scoreTimer -= Time.deltaTime;
+        point = point + 100; //will delete later
+        if (scoreTimer < 0) 
+        {    
+            scoreTimer = scoreDelayTimer;
+            scoreText.text = point.ToString();  
+        }
         checkEvent(point);
-        
+
     }
     public void checkEvent(int point) {
-        if (point >= 10000 && point <= 10500)
+        if (point >= 10000 && point <= 10500) //optimization needed so boss does not appear multiple times
         {
-            Debug.Log("The First Boss Appears"); // work in progress
+            Debug.Log("The First Boss Appears"); // work in progress, plan to have boss events, and text that appears
         }
 
         else if (point >= 20000 && point <= 20500)
         {
-            Debug.Log("The Second Boss Appears"); // work in progress
+            Debug.Log("The Second Boss Appears"); // work in progress, plan to have boss events, and text that appears
         }
         else if (point >= 30000 && point <= 30500)
         {
-            Debug.Log("The Third Boss Appears"); // work in progress
+            Debug.Log("The Third Boss Appears"); // work in progress, plan to have boss events, and text that appears
         }
         else if (point >= 40000 && point <= 40500)
         {
-            Debug.Log("The Fourth Boss Appears"); // work in progress
+            Debug.Log("The Fourth Boss Appears"); // work in progress, plan to have boss events, and text that appears
         }
         else if (point >= 50000 && point <= 50500)
         {
-            Debug.Log("The Fifth Boss Appears"); // work in progress
+            Debug.Log("The Fifth Boss Appears"); // work in progress, plan to have boss events, and text that appears
         }
 
     }
