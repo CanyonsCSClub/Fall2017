f24d0dc5ac8acdcc38c590e7c212d8180d86f710
Jack Bruce <jackbruce0@gmail.com>
2017-10-27 11:26:18 -0700

    Asteroid Update
    Movement and rotation!

---
+++ CompSciClubFall2017/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+    public float speed;
+    public float tumble;
+    //public float xpos = Random.Range(-5, 5);
+    //public Vector3 spawnLocation = new Vector3(0f, 0.5f, 0f);
+
+    void Start()
+    {
+
+        //random position
+        //GetComponent<Rigidbody>().position = spawnLocation;
+
+        //straight forward movement
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        //random rotation
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+
+    }
+
+
+}
+
+    // called once per frame
+    //void Update() {
+        //if off the plane, asteroid is gonna die
+
+    //}
+
+
