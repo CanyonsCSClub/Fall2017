f516b13d07d7457dfee068396fb1c483833ea387
Unknown <kgmaki@my.canyons.edu>
2017-10-27 10:30:22 -0700

    Issue-#13
    Created the Enemy.cs script that will be the parent class for all enemy child classes.
    
    Created EnemyDevelopment scene.

---
+++ CompSciClubFall2017/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * 
+ * Authors: Keaton Maki
+ * Date Created: 10/25/2017 @ 5:29 pm
+ * Date Modified: 10/25/2017 @ 5:04 pm
+ * Project: CompSciClubFall2017
+ * File: Enemy.cs
+ * Description: Parent class for all enemy child classes.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Enemy: MonoBehaviour{
+    //values that can be applied to any enemy child. 
+    //private float damage; 
+    private float health;
+    private float speed;
+
+    //private WeaponType weapon;
+
+    //damage = default;
+    //health = default;
+    //speed = default;
+
+    //Setters
+    /*
+    public void setDamage(float newDamage){
+        damage = newDamage;
+    }
+    */
+
+    public void setHealth(float newHealth){
+        health = newHealth;
+    }
+    public void setSpeed(float newSpeed){
+        speed = newSpeed;
+    }
+
+    //Getters
+    /*public float getDamage(){
+        return damage;
+    }
+    */
+    public float getHealth(){
+        return health;
+    }
+    public float getSpeed(){
+        return speed;
+    }
+
+}
