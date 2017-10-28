e2f346aa3f8104ccbe933a6373c774fd9dd4b940
Jack Bruce <jackbruce0@gmail.com>
2017-10-28 12:07:54 -0700

    Asteroid Update
    Added asteroid model
    game controller object and script for 2.5d space

--- CompSciClubFall2017/Assets/Scripts/Asteroid.cs
+++ CompSciClubFall2017/Assets/Scripts/Asteroid.cs
@@ -1,16 +1,37 @@
+<<<<<<< Updated upstream
 ﻿using UnityEngine;
+=======
+﻿/*
+ * Asteroid.cs
+ * Created by: Jack Bruce
+ * Description: Asteroid movement controller
+ */
+
+
+using UnityEngine;
+>>>>>>> Stashed changes
 using System.Collections;
 
 public class Asteroid : MonoBehaviour
 {
     public float speed;
     public float tumble;
+<<<<<<< Updated upstream
     //public float xpos = Random.Range(-5, 5);
     //public Vector3 spawnLocation = new Vector3(0f, 0.5f, 0f);
 
     void Start()
     {
 
+=======
+    //static public float xpos = Random.Range(-5f, 5f);
+    //public Vector3 spawnLocation = new Vector3(xpos, 0.5f, 0f);
+
+
+    void Start()
+    {
+        
+>>>>>>> Stashed changes
         //random position
         //GetComponent<Rigidbody>().position = spawnLocation;
 
---
+++ CompSciClubFall2017/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * GameController.cs
+ * Created By: Jack Bruce
+ * Description: Controlling gameobjects of things that spawn and hurt player
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard; // any enemy or asteroid
+    public Vector3 spawnValues;
+
+    void Start() {
+        SpawnWaves();
+    }
+
+    void SpawnWaves () {
+        Vector3 spawnPosition = new Vector3();
+        Quaternion spawnRotation = new Quaternion();
+        Instantiate(hazard, spawnPosition, spawnRotation); 
+    }
+}
