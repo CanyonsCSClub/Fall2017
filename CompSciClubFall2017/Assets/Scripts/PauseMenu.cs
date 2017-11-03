a3030d0490993fc02f335e4cc8af66ce10574b59
Evan <esgierst@gmail.com>
2017-11-03 11:56:42 -0700

    Rudimentary pause
    this is bad dont use this
    its not done yet

---
+++ CompSciClubFall2017/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isResume;
+    public bool isReturnToMenu;
+
+    void OnMouseUp()
+    {
+        if (isResume)
+        {
+            Time.timeScale = 1;
+            Application.LoadLevel(1);
+            // How to unpause
+        }
+        if (isReturnToMenu)
+        {
+            Time.timeScale = 1;
+            Application.LoadLevel(0);
+        }
+    }
+}
