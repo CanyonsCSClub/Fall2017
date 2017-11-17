cd8c8fa31e99cd0a00170a0373193099135788ea
Evan <esgierst@gmail.com>
2017-11-17 11:42:08 -0800

    I have to remake this by putting it into the main scene

---
+++ CompSciClubFall2017/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    public bool isResume;
+    public bool isReturn;
+
+    void OnMouseUp() {
+        if (isResume)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(2);
+            // How to unpause
+        }
+        if (isReturn)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
