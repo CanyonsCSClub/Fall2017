66b4931563a4b64350f591200ff31961f64fc7a9
Persomatey <huntergoodin1@hotmail.com>
2017-11-22 19:32:21 -0800

    Made Level & Implimented Models
    I made a scene as a level where everyone can play the game. We will use this scene for initialization so be careful about editing it. I also implimented some FBX models into Unity. We will be using the FBX models for all models in the game. We will NOT be using the .mb files, only the .fbx files as some machines won't render the .mb files properly and Unity supports .fbx files natively.

---
+++ CompSciClubFall2017/Assets/Scripts/BoltMover.cs
@@ -0,0 +1,58 @@
+﻿/* 
+ * Programmer:	Hunter Goodin 
+ * Date:		11/5/2017 
+ * Project: 	CompSciClubFall2017
+ * Description: Creating the Bolt object the player will shoot. 
+ */
+
+using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+public class BoltMover : MonoBehaviour
+{
+    public float speed;             // The speed at which the Bolt moves. This float can be accessed in-engine when inspecting the Bolt prefab. 
+    public int damage = 1;
+
+    private Rigidbody rb;           // Creating the Rigidbody for this script named 'rb'. 
+    private float lifetime = 1.5f;    // We will use this to destory our bolts after 2 second. 
+
+    void Start()			// The following code will occur the frame a Bolt is created. 
+    {
+        rb = GetComponent<Rigidbody>(); 			 // rb is set to Bolt's Rigidbody established in-engine. 
+        rb.velocity = transform.right * speed;       // The Rigidbody will move right based on the speed float. 
+        Destroy(gameObject, lifetime);               // Destroys this object based on our lifetime float we created earlier. 
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Stinger")
+        {
+            GameObject.Find("Stinger").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "Stinger (1)")
+        {
+            GameObject.Find("Stinger (1)").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "Stinger (2)")
+        {
+            GameObject.Find("Stinger (2)").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "Stinger (3)")
+        {
+            GameObject.Find("Stinger (3)").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "Stinger (4)")
+        {
+            GameObject.Find("Stinger (4)").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "Stinger (5)")
+        {
+            GameObject.Find("Stinger (5)").GetComponent<StingerSuperClass>().takeDamage(damage);
+        }
+        else if (col.gameObject.name == "StingerBullet(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ CompSciClubFall2017/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,29 @@
+﻿/* 
+ * Programmer:	Hunter Goodin 
+ * Date:		10/20/2017
+ * Project: 	SpaceShooterTutorial 
+ * Description: This is almost a carbon copy of Evan's MainMenu.cs script. 
+ *              This will be used for the game's "Game Over" screen. 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel(1);   // This loads the scene set as the first level 
+        }
+        if (isQuit)
+        {
+            Application.Quit();         // This exits the game 
+        }
+    }
+}
