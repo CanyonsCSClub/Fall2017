3c213e113d4c20e5c62b096390a883752407955d
Persomatey <huntergoodin1@hotmail.com>
2017-12-01 12:12:24 -0800

    undo (#56)
    * undo
    
    * Fix Everything Take Two
    
    ^^^

--- CompSciClubFall2017/Assets/Scripts/Player.cs
+++ CompSciClubFall2017/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
  * 
  * Authors: Spencer Wilson, Andrew Ramirez, Hunter Goodin
  * Date Created: 10/8/2017 @ 5:29 pm
- * Date Modified: 11/20/2017 @ 9:13 pm
+ * Date Modified: 11/20/2017 @ 10:06 pm by Hunter Goodin 
  * Project: CompSciClubFall2017
  * File: Player.cs
  * Description: File that houses all of the code for the player's health, lives, movement, and abilities.
@@ -21,6 +21,7 @@ public class Player : MonoBehaviour {
     public float speed; // Declared a floating point variable named speed that will house the user's speed.
     private Rigidbody player; // Creating a Rigidbody object named player.
     private bool isEnemy;
+    public Boundary boundary; 
 
     /* Code regarding player's health and lives */
     // private int currentLives;
@@ -37,11 +38,10 @@ public class Player : MonoBehaviour {
     public float fireRate;          // The player's fire rate -- this will probably kept as 0 but... just in case. 
     private float nextFire;         // Will be used for the next time the player can fire a shot.
 
-
     public void Start () // Start method initializes any variables/objects/rigidbodies that need to be used within the script.
     {
         player = GetComponent<Rigidbody>(); // Getting the Rigidbody component of the GameObject player is attached to.
-        // speed = 40f; // Initializing the speed variable with an value of 40.
+        speed = 40f; // Initializing the speed variable with an value of 40.
         isEnemy = false; // Sets the player as not an enemy
         //currentLives = 2; // Set initial player live count
         Health playerHealth = this.GetComponent<Health>();
@@ -140,6 +140,12 @@ public class Player : MonoBehaviour {
         }
     }
 
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax, yMin, yMax;    // initializes these values 
+    }
+
     private void playerMovement()
     {
         if(Input.GetKey("w")) // When w is pressed, move the player up.
@@ -162,6 +168,13 @@ public class Player : MonoBehaviour {
             player.transform.Translate(transform.right * Time.deltaTime * speed);
             //Debug.Log("Player is moving to the left.");
         }
+        //player.position = new Vector3(transform.position.x, transform.position.y, 0f);
+        player.position = new Vector3
+        (
+            Mathf.Clamp (player.position.x, boundary.xMin, boundary.xMax),  // The player can't move beyond these values on the X axis 
+            Mathf.Clamp (player.position.y, boundary.yMin, boundary.yMax),  // The player can't move beyond these values on the Y axis
+            0.0f                                                            // The player can't move beyond 0.0 on the Z axis 
+        ); 
     }
 
     private void isAlive()
