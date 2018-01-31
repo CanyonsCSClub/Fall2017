619ad18d1e0d1e8c18866a7ac270ba4762f8fb71
Unknown <spencerewilson1@gmail.com>
2018-01-30 17:32:15 -0800

    Set up the Flash On Damage files
    Set up the flash on damage files.

---
+++ CompSciClubFall2017/Assets/Scripts/Enemy Scripts/FlashOnDamage.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+ * Author: Spencer Wilson
+ * Date Created: 1/29/2018 @ 9:02 pm
+ * Date Modified: 1/29/2018 @ 9:02 pm
+ * Project: CompSciClubFall2017
+ * File: EnemyFlashOnDamage.cs
+ * Description: This file contains the code for the enemy's flash upon being hit by the player's weaponry.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashOnDamage : MonoBehaviour {
+
+    IEnumerator Flash()
+    {
+        Debug.Log("Flash!"); //https://answers.unity.com/questions/838194/make-your-player-flash-when-hit.html
+    }
+}
---
+++ CompSciClubFall2017/Assets/Scripts/Player/BoltMover.cs
@@ -0,0 +1,63 @@
+﻿/* 
+ * Programmer:	Hunter Goodin 
+ * Date:		11/5/2017 
+ * Project: 	CompSciClubFall2017
+ * Description: Creating the Bolt object the player will shoot. 
+ */
+
+using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+public class BoltMover : MonoBehaviour
+{
+    public float speed;             // The speed at which the Bolt moves. This float can be accessed in-engine when inspecting the Bolt prefab. 
+    public int damage = 1;
+
+    private Rigidbody rb;           // Creating the Rigidbody for this script named 'rb'. 
+    private float lifetime = 1.5f;    // We will use this to destory our bolts after 2 second. 
+
+    void Start()			// The following code will occur the frame a Bolt is created. 
+    {
+        rb = GetComponent<Rigidbody>(); 			 // rb is set to Bolt's Rigidbody established in-engine. 
+        rb.velocity = transform.right * speed;       // The Rigidbody will move right based on the speed float. 
+        Destroy(gameObject, lifetime);               // Destroys this object based on our lifetime float we created earlier. 
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        // Stinger 
+        if(col.gameObject.tag == "Stinger")
+        {
+            col.gameObject.GetComponent<StingerClass>().TakeDamage(damage);
+            col.gameObject.GetComponent<FlashOnDamage>().Flash(); // 
+        }
+        // Chomper 
+        else if (col.gameObject.tag == "Chomper")
+        {
+            col.gameObject.GetComponent<ChomperClass>().takeDamage(damage);
+            col.gameObject.GetComponent<FlashOnDamage>().Flash();
+        }
+        // Crabber 
+        else if (col.gameObject.tag == "Crabber")
+        {
+            col.gameObject.GetComponent<CrabberClass>().takeDamage(damage);
+            col.gameObject.GetComponent<FlashOnDamage>().Flash();
+        }
+        // Striker 
+        else if (col.gameObject.tag == "Striker")
+        {
+            col.gameObject.GetComponent<StrikerClass>().TakeDamage(damage);
+            col.gameObject.GetComponent<FlashOnDamage>().Flash();
+        }
+        // Asteroids 
+        else if (col.gameObject.tag == "astroid")
+        {
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.name == "StingerBullet(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
